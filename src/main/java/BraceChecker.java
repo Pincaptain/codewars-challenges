0d98a945e939313f90b32e77f1df4786917c98e7
Borjan Gjorovski <borjan.gjorovski@outlook.com>
2022-04-05 01:57:41 +0200

    Implement BraceChecker

---
+++ src/main/java/BraceChecker.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class BraceChecker {
+
+    public boolean isValid(String braces) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : braces.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(' || c == '}' && top != '{' || c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
