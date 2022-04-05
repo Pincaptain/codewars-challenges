f232b64a4ac418f39d4de3fb1a64c9f29b7dd881
Borjan Gjorovski <borjan.gjorovski@outlook.com>
2022-04-05 12:58:42 +0200

    Refactor BuildTower

--- src/main/java/BuildTower.java
+++ src/main/java/BuildTower.java
@@ -3,17 +3,10 @@ public class BuildTower {
     public static String[] buildTower(int floors) {
         String[] tower = new String[floors];
         for (int i = 0; i < floors; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < floors - i - 1; j++) {
-                sb.append(" ");
-            }
-            for (int j = 0; j < i * 2 + 1; j++) {
-                sb.append("*");
-            }
-            for (int j = 0; j < floors - i - 1; j++) {
-                sb.append(" ");
-            }
-            tower[i] = sb.toString();
+            String sb = " ".repeat(floors - i - 1) +
+                    "*".repeat(Math.max(0, i * 2 + 1)) +
+                    " ".repeat(floors - i - 1);
+            tower[i] = sb;
         }
         return tower;
     }
