8433da2fe5ea6fbc36f6fd405c0a89423058aa99
Borjan Gjorovski <borjan.gjorovski@outlook.com>
2022-06-26 14:19:45 +0200

    Solve some simple problems

---
+++ src/main/java/HammingNumbers.java
@@ -0,0 +1,10 @@
+public class HammingNumbers {
+
+    private static long hamming(int n) {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hamming(3));
+    }
+}
